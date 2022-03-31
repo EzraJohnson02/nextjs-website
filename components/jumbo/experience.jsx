becaaffe033337c4ebd1746d7363eb054d261890
ezrajohnson <erjohnso@caltech.edu>
2022-03-30 23:50:34 -0400

    completed experience page

---
+++ components/jumbo/experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Experience() {
+  return (
+    <section className="jumbotron text-center">
+      <div className="container">
+        <h1>My Previous Industry Work</h1>
+      </div>
+    </section>
+  )
+}
--- pages/experience.jsx
+++ pages/experience.jsx
@@ -1,38 +1,40 @@
+import Link from 'next/link'
 import SimpleLayout from '../components/layout/simple'
-import Carousel from 'react-bootstrap/Carousel'
-import { CarouselItem } from 'react-bootstrap'
+import ExperienceJumbo from '../components/jumbo/experience'
 
 export default function Experience() {
   return (
 
-    <SimpleLayout>
+
+    <SimpleLayout preContainer={<ExperienceJumbo />}>
       <div className="row">
-        Experience Form
+        <div className="col-md-4">
+          <div className="card mb-4 shadow-sm">
+            <img
+              className="d-block w-100"
+              src="/google-lens.png"
+              alt="Second slide"
+            />
+            <div className="card-body">
+              <h3>Google STEP</h3>
+              <p className="card-text">I worked on the Lens in Photos team at Google through their STEP internship in the summer of 2021.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card mb-4 shadow-sm">
+          <img
+              className="d-block w-100"
+              src="/meta-logo.png"
+              alt="Second slide"
+            />
+            <div className="card-body">
+              <h3>Meta SWE</h3>
+              <p className="card-text">Upcoming!</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <Carousel variant="dark">
-        <CarouselItem style={{ 'height': "800px" }} >
-          <img
-            className="d-block w-100"
-            src="src/screenshot-articles.png"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </CarouselItem>
-        <CarouselItem style={{ 'height': "400px" }} >
-          <img
-            className="d-block w-100"
-            src="src/screenshot-articles.png"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </CarouselItem>
-      </Carousel>
     </SimpleLayout>
   )
 }
